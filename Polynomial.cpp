32d3c671c79f59e2e0396ef427916277504724d4
garrett1001 <31227173+garrett1001@users.noreply.github.com>
2018-10-03 01:12:14 -0700

    Update Polynomial.cpp

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -1,3 +1,32 @@
 #include "Polynomial.h"
 #include "Node.h"
 #include <cstddef>
+#include <iostream>
+
+using namespace std;
+
+template<class ItemType>
+Polynomial<ItemType>::Polynomial()
+{
+    headPtr = nullptr;
+}
+
+template<class ItemType>
+void Polynomial<ItemType>::insert( const ItemType& newEntry )
+{
+    Node<ItemType>* nextPtr = new Node<ItemType>(newEntry, headPtr);
+    headPtr = nextPtr;
+}
+
+template<class ItemType>
+void Polynomial<ItemType>::print()
+{
+    Node<ItemType>* curPtr = headPtr;
+
+    while (curPtr != nullptr)
+    {
+        cout<<curPtr->getItem()<<endl;
+
+        curPtr = curPtr->getNext();
+    }
+}
