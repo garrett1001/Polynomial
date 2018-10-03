2810fc7bf73f211d77fb98316850dfb5a2208e18
garrett1001 <31227173+garrett1001@users.noreply.github.com>
2018-10-03 01:14:47 -0700

    Update Polynomial.h

--- Polynomial.h
+++ Polynomial.h
@@ -3,19 +3,18 @@
     
 #include "Node.h"
 
+template<class ItemType>
 class Polynomial
 {
-private:
+    private:
+        Node<ItemType>* headPtr; // Pointer to first node
+        int polyCount;
 
-Node<ItemType>* headPtr; // Pointer to first node
-int polyCount;
-
-public:
-
-Polynomial();
-void insert( const ItemType& newEntry ); // Insert at beginning
-void print(); // Display to standard output
-
+    public:
+        Polynomial();
+        void insert( const ItemType& newEntry ); // Insert at beginning
+        void print(); // Display to standard output
 };
 
+#include"Polynomial.cpp"
 #endif
