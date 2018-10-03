2c8c6eb7d078976855782b6f549e48557bafe105
garrett1001 <31227173+garrett1001@users.noreply.github.com>
2018-10-02 22:39:23 -0700

    Update Polynomial.h

--- Polynomial.h
+++ Polynomial.h
@@ -1,1 +1,18 @@
+#ifndef _POLYNOMIAL_H_
+#define _POLYNOMIAL_H_
+    
+#include "Node.h"
 
+class polynomial
+{
+private:
+
+Node<ItemType>* headPtr; // Pointer to first node
+int polyCount;
+
+public:
+
+};
+
+#include "Polynomial.cpp"
+#endif
